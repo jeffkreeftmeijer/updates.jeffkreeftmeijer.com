cf6ecd28fbf9eb5e25268b33893bd195022d61f0
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-12-16 13:04:12 +0100

    Add <wbr>s after slashes in urls

--- _plugins/short_urls.rb
+++ _plugins/short_urls.rb
@@ -1,5 +1,9 @@
 require "jekyll"
 
 Jekyll::Hooks.register [:pages, :documents], :post_render do |document|
-  document.output.gsub!(/<a href="([^"]+")>(\w+:\/\/(w{3}\.)?)([^<]+)<\/a>/, '<a href="\\1"><span class="invisible">\\2</span>\\4</a>')
+  document.output.gsub!(/<a href="([^"]+")>(\w+:\/\/(w{3}\.)?)([^<]+)<\/a>/) do
+    match = Regexp.last_match
+    url = match[4].gsub("/", "/<wbr>")
+    %(<a href="#{match[1]}"><span class="invisible">#{match[2]}</span>#{url}</a>)
+  end
 end
