821aceb0066057a01a94805fec803a254a7f17a8
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-12-19 21:13:57 +0100

    Only oembed links at the end of the update

--- _plugins/oembed.rb
+++ _plugins/oembed.rb
@@ -2,7 +2,7 @@ require 'jekyll'
 require 'net/http'
 
 Jekyll::Hooks.register :posts, :pre_render do |post|
-  post.content.gsub!(/<(https:\/\/twitter\.com\/\w+\/status\/\w+)>/) do |link|
+  post.content.gsub!(/\n\n<(https:\/\/twitter\.com\/\w+\/status\/\w+)>$/) do |link|
     match = Regexp.last_match
 
     uri = URI("https://publish.twitter.com/oembed?url=#{match[1]}")
