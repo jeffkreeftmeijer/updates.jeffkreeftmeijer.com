dce98cde105abd64b0039c1b01d486955f2a7812
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-12-19 21:13:43 +0100

    Don't hide oembed links

--- _plugins/oembed.rb
+++ _plugins/oembed.rb
@@ -2,12 +2,12 @@ require 'jekyll'
 require 'net/http'
 
 Jekyll::Hooks.register :posts, :pre_render do |post|
-  post.content.gsub!(/<(https:\/\/twitter\.com\/\w+\/status\/\w+)>/) do
+  post.content.gsub!(/<(https:\/\/twitter\.com\/\w+\/status\/\w+)>/) do |link|
     match = Regexp.last_match
 
     uri = URI("https://publish.twitter.com/oembed?url=#{match[1]}")
     data = JSON.parse(Net::HTTP.get(uri))
 
-    '<article>' + data['html'].gsub(/<script.+<\/script>/, "") + '</article>'
+    link + ":\n" + '<article>' + data['html'].gsub(/<script.+<\/script>/, "") + '</article>'
   end
 end
